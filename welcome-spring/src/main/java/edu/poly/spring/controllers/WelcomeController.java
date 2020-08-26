7506860f10c6678f0e9b1b88ecc5f5e1d932eca3
tompetrel <huutaimarvel@gmail.com>
2020-08-26 21:47:18 +0700

    Spring Web

--- welcome-spring/src/main/java/edu/poly/spring/controllers/WelcomeController.java
+++ welcome-spring/src/main/java/edu/poly/spring/controllers/WelcomeController.java
@@ -1,12 +1,15 @@
 package edu.poly.spring.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WelcomeController {
 	@RequestMapping("/welcome")
-	public String showForm() {
+	public String showForm(HttpServletRequest request) {
+		request.setAttribute("name", "dotaizzzzzzzzzzzzzzzzzzzzzzzzzaaaaaaaaaaaaaaaaaaaaaaaa");
 		return "welcome";
 	}
 }
